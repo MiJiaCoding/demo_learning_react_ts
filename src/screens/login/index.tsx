d35cdc91715adfab8b90459c0809c674437bdd03
MiJia <107978955+MiJiaCoding@users.noreply.github.com>
2023-05-16 20:35:21 +0800

    feat: add login

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const LoginScreen = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLInputElement).value //as HTMLInputElement:不加会报错类型“Element”上不存在属性“value”。
+  };
+  fetch(`${apiUrl}/login}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(async (response) => {
+    if (response.ok) {
+    }
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" id={"username"}></input>
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input type="password" id={"password"}></input>
+      </div>
+      <button type={"submit"}>登录</button>
+    </form>
+  );
+};
+
+export default LoginScreen;
---
+++ src/utils/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === "";
+
+export const cleanObject = (object?: { [key: string]: unknown }) => {
+  // Object.assign({}, object)
+  if (!object) {
+    return {};
+  }
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isVoid(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+// 后面用泛型来规范类型
+export const useDebounce = <V,>(value: V, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+};
