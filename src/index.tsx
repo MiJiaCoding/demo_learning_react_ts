c1a6f09f5950184aeac0a8351d1bff422e8751c0
MiJia <107978955+MiJiaCoding@users.noreply.github.com>
2023-05-19 21:00:25 +0800

    test: prettier 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import React,{useEffect,usestate} from 'react'
+import React,{useEffect,usestate,useCallback} from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+   
+
 
 
 
