f566d489ac7dba27ab6ede0a2256ddbcfb5470e5
MiJia <107978955+MiJiaCoding@users.noreply.github.com>
2023-05-19 21:19:11 +0800

    test: prettier & husky 3

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React,{useEffect,useState,useCallback,useContext} from 'react'
+import React,{useEffect,useState,useCallback} from 'react'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
     
+
 
 
 
