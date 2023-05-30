fdb27116137cc0738bb2a4393a740b12415cba4a
MiJia <107978955+MiJiaCoding@users.noreply.github.com>
2023-05-30 22:24:31 +0800

    style: customize Row style component layout pages using emotion

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,3 +1,5 @@
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectList } from "screens/projectList";
 
@@ -5,8 +7,41 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
-      <button onClick={logout}>登出</button>
-      <ProjectList />
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectList />
+      </Main>
     </div>
   );
 };
+
+// temporal dead zone(暂时性死区)
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+// grid-area 用来给grid子元素起名字
+
+const Header = styled(Row)`
+  /* padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1; */
+`;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  /* display: flex;
+  overflow: hidden; */
+`;
